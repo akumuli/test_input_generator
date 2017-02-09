142e571476b2d47ab13e47d5b6183b55cc67b1f2
Eugene Lazin <4lazin@gmail.com>
2017-02-09 11:40:38 -0800

    WIP: fake measurements generation

--- main.py
+++ main.py
@@ -27,23 +27,59 @@ def bulk_msg(ts, measurements, values, **tags):
     return '\r\n'.join(lines) + '\r\n'
 
 def generate_rows(ts, measurements, delta, **tags):
-	row = [0.0] * len(measurements)
-	
-	while True:
-		for i in xrange(0, len(measurements)):
-			row[i] += random.normal(0.0, 0.01, 0.01)
-		msg = bulk_msg(ts, measurements, row, **tags)
-		yield ts, msg
-		ts += delta
-	
+    row = [0.0] * len(measurements)
+
+    while True:
+        for i in xrange(0, len(measurements)):
+            row[i] += random.normal(0.0, 0.01, 0.01)
+            msg = bulk_msg(ts, measurements, row, **tags)
+            yield ts, msg
+            ts += delta
+
+def generate_rr(list_fn):
+    N = len(list_fn)
+    iters = [fn() for fn in list_fn]
+    ix = 0
+    while True:
+        it = iters[ix % N]
+        yield it.next()
+        ix += 1
 
 
 def main(idrange, timerange):
     print("Ids  - [{0}, {1}]".format(*idrange))
     print("Time - [{0}, {1}, {2}]".format(*timerange))
 
+    begin, end, delta = timerange
+    idbegin, idend = idrange
 
+    measurements = [
+        'cpu.user', 'cpu.sys', 'cpu.real', 'idle', 'mem.commit',
+        'mem.virt', 'iops', 'tcp.packets.in', 'tcp.packets.out',
+        'tcp.ret', 'pg.mem', 'pg.ncon'
+    ]
 
+    tag_combinations = {
+        'region': ['NW', 'NE', 'Europe'],
+        'OS': ['Ubuntu 16.04', 'Ubuntu 14.04']
+        'instance-type': ['m3.large', 'm3.xlarge', 'm3.2xlarge', 'm4.large', 'm4.xlarge', 'm4.2xlarge']
+    }
+
+    list_hosts = ['host_{0}'.format(id) for id in range(idbegin, idend)]
+
+    tags = []
+    for host in list_hosts:
+        tagline = {'host': host}
+        for k, v in tag_combinations.iteritems():
+            tagline[k] = random.chose(v)
+        tags.append(tags)
+
+    lambdas = [lambda: generate_rows(begin, delta, measurements, **t) for t in tags]
+
+    for ts, msg in generate_rr(lambdas):
+        if ts > end:
+            break
+        print(msg)
 
 
 if __name__ == '__main__':
